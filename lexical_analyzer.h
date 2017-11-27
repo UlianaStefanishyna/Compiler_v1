b2310893bca032531b51bb271d9f6f52aadf38a0
uliana <ulanka1996@gmail.com>
2017-11-28 01:41:44 +0200

    complete lexical analyzer

---
+++ lexical_analyzer.h
@@ -0,0 +1,46 @@
+//
+// Created by uliana on 28/11/17.
+//
+#include <iostream>
+#include <fstream>
+#include "string"
+#include "Token.h"
+using namespace std;
+
+ifstream openFile(string filename);
+
+void lexical_analyzer(string filename){
+    ifstream inFile = openFile(filename);
+
+    Token token;
+    char ch;
+    string str;
+    while (inFile.get(ch)) {
+        bool fDel = false;
+        for (char temp : token.getM_Delimitations()) {
+            if (temp == ch) {
+                token.addDelimiter(ch);
+                if(str != "")
+                    token.addToken(str);
+                str = "";
+                fDel = true;
+                break;
+            }
+        }
+        if(fDel)
+            continue;
+        str += ch;
+    }
+}
+
+ifstream openFile(string filename) {
+    ifstream inFile;
+    inFile.open(filename);
+    string x = "";
+
+    if (!inFile) {
+        cout << "Unable to open file";
+        exit(1); // terminate with error
+    }
+    return inFile;
+}
--- main.cpp
+++ main.cpp
@@ -1,45 +1,11 @@
-#include <iostream>
-#include <fstream>
-#include "string"
-#include "Token.h"
-
-using namespace std;
-
-ifstream openFile(string filename);
+#include "lexical_analyzer.h"
 
 int main() {
 
-    ifstream inFile = openFile("test");
-    Token token;
-    char ch;
-    string str;
-    while (inFile.get(ch)) {
-        bool fDel = false;
-        for (char temp : token.getM_Delimitations()) {
-            if (temp == ch) {
-                token.addDelimiter(ch);
-                if(str != "")
-                    token.addToken(str);
-                str = "";
-                fDel = true;
-                break;
-            }
-        }
-        if(fDel)
-            continue;
-        str += ch;
-    }
+
+    string filename = "test";
+    lexical_analyzer(filename);
+
     return 0;
 }
 
-ifstream openFile(string filename) {
-    ifstream inFile;
-    inFile.open(filename);
-    string x = "";
-
-    if (!inFile) {
-        cout << "Unable to open file";
-        exit(1); // terminate with error
-    }
-    return inFile;
-}
